9c91ac92df10edcd606e89d44447827695eca222
chunmin <chun.m.chang@gmail.com>
2018-09-09 10:21:16 -0700

    code for step 2

--- rust_audio_lib/src/main.rs
+++ rust_audio_lib/src/main.rs
@@ -1,3 +1,16 @@
+extern "C" {
+    fn abs(input: i32) -> i32;
+}
+
+// A wrapper for native C API.
+fn get_abs(x: i32) -> i32 {
+    unsafe {
+        abs(x)
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let x: i32 = -50;
+    let abs: i32 = get_abs(x);
+    println!("abs of {} is {}", x, abs);
 }
